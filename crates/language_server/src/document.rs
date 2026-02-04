3d2135fd789a0d30672889926599ad091bbad1be
widsnoy <ricofx47@outlook.com>
2026-02-04 20:06:23 +0800

    feat(ls): semantic token

--- crates/language_server/src/document.rs
+++ crates/language_server/src/document.rs
@@ -1,11 +1,12 @@
 use analyzer::module::Module;
+use line_index::LineIndex;
 use parser::parse::Parser;
-
-use crate::utils::line_index::LineIndex;
+use tower_lsp_server::ls_types::SemanticToken;
 
 /// 文档状态管理
 #[derive(Debug)]
 pub struct Document {
+    #[allow(unused)] // FIXME
     text: String,
     line_index: LineIndex,
     module: Module,
@@ -15,7 +16,15 @@ impl Document {
     /// 创建新文档
     pub fn new(text: String) -> Self {
         let parser = Parser::new(&text);
-        let line_index = LineIndex::new(parser.get_newline_end_postions());
+        let line_index = LineIndex::new(
+            parser
+                .lexer
+                .get_tokens()
+                .iter()
+                .filter(|(kind, _, _)| *kind == syntax::SyntaxKind::NEWLINE)
+                .map(|(_, _, r)| r.end)
+                .collect::<Vec<_>>(),
+        );
 
         let (green_node, _parser_errors) = parser.parse(); //FIXME:
         let mut module = Module::new(green_node);
@@ -29,6 +38,14 @@ impl Document {
 
     /// 更新文档内容
     pub fn update(&mut self, text: String) {
-        self.text = text;
+        *self = Self::new(text);
+    }
+
+    /// 计算语义 tokens
+    pub fn compute_semantic_tokens(&self) -> Vec<SemanticToken> {
+        crate::lsp_features::semantic_tokens::compute_semantic_tokens(
+            &self.module,
+            &self.line_index,
+        )
     }
 }
