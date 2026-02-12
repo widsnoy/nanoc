017fa42443d06d744843516b02d9ffc84521c720
widsnoy <ricofx47@outlook.com>
2026-02-12 19:37:41 +0800

    remove: runtime crate

--- crates/runtime/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    cc::Build::new().file("src/sylib.c").compile("sylib");
-}
--- crates/runtime/src/lib.rs
+++
@@ -1,2 +0,0 @@
-// This crate is primarily a container for the C runtime library.
-// The build script compiles the C code.
